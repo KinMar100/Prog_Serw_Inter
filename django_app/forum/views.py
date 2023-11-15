2db1a3efde1f8be9907aad52c674d99049066280
KinMar100 <markowska.kinga13@gmail.com>
2023-11-15 16:07:23 +0100

    my_app

--- django_app/forum/views.py
+++ django_app/forum/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse as HttpR, HttpResponseBase
 
+from .models import Rank
+
+# ranks = [
+#    {'id': 1, 'name': 'first'},
+#    {'id': 2, 'name': 'second'},
+#    {'id': 3, 'name': 'third'},
+# ]
+
 
 def home(request) -> HttpResponseBase:
-    return render(request, 'home.html')
+    ranks = Rank.objects.all()
+    # wszystkie rangi w bazie
+    context = {'ranks': ranks}
+    return render(request, 'home.html', context)
 
 
-def subpage(request) -> HttpResponseBase:
-    return render(request, 'subpage.html')
+def subpage(request, pk) -> HttpResponseBase:
+    rank = Rank.objects.get(id=pk)
+    context = {'rank': rank}
+    return render(request, 'subpage.html', context)
