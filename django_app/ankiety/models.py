75a8d3432435f97c8d976a9b9b2ccbe1fa21a8cd
KinMar100 <markowska.kinga13@gmail.com>
2023-11-28 23:35:44 +0100

    my_app_add_models

--- django_app/ankiety/models.py
+++ django_app/ankiety/models.py
@@ -1,14 +1,27 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
+# model - Pytanie
 class Pytanie(models.Model):
     pytanie_tresc = models.CharField(max_length=250)
     data_publikacji = models.DateTimeField("opublikowane")
 
+    def __str__(self) -> str:
+        return self.pytanie_tresc
 
+    def czy_opublikowane_niedawno(self) -> bool:
+        return self.data_publikacji >= timezone.now() - datetime.timedelta(days=1)
+
+
+# model - Wybor (Klucz obcy do modelu - Pytanie)
 class Wybor(models.Model):
     pytanie = models.ForeignKey(Pytanie, on_delete=models.CASCADE)
     wybor_tresc = models.CharField(max_length=200)
     glosy = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.wybor_tresc
--- django_app/ankiety/views.py
+++ django_app/ankiety/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse as HttpR
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse as HttpR, Http404
+
+from .models import Pytanie, Wybor
+
 
 # Create your views here.
 
 
-def index(request):
+def index(request) -> HttpR:
     return HttpR("Indeks ankiet")
+
+
+def szczegoly(request, pytanie_id) -> HttpR:
+    try:
+        pytanie = Pytanie.objects.get(pk=pytanie_id)
+    except Pytanie.DoesNotExist:
+        raise Http404("Takie pytanie nie istnieje.")
+    return render(request, {"question": pytanie})
+
+
+def wynik(request, pytanie_id) -> HttpR:
+    try:
+        pytanie = Pytanie.objects.get(pk=pytanie_id)
+    except Pytanie.DoesNotExist:
+        raise Http404("Takie pytanie nie istnieje.")
+    return render(request, {"question": pytanie})
+    # return HttpR("Wyszukiwanie wyniku dla pytania: {}".format(pytanie_id))
+
+
+def glos(request, pytanie_id) -> HttpR:
+    question = get_object_or_404(Pytanie, pk=pytanie_id)
+    try:
+        wybrany_wybor = pytanie_id.choice_set.get(pk=request.POST["wybor"])
+    except (KeyError, Wybor.DoesNotExist):
+        return render(request, {"pytanie": question, "error_message": "Nie wybrano wyboru."},)
+    else:
+        wybrany_wybor.votes += 1
+        wybrany_wybor.save()
+    # return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
