8245c42c60dc7c7a7c531afe6bcd1ac022a509a8
KinMar100 <markowska.kinga13@gmail.com>
2023-12-05 20:01:15 +0100

    admin panel

--- django_app/polls/admin.py
+++ django_app/polls/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from . models import Question, Choice, Category
+from .models import Question, Choice, Category
+
 
 # Register your models here.
+# admin panel
 
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Category)
+class ChoicesInLine(admin.StackedInline):
+    model = Choice
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['id', 'name', 'question_text', 'category']
+    list_filter = ['name']
+    inlines = [ChoicesInLine]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'choice_text', 'question', 'votes']
+    list_filter = ['choice_text']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 # done
--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Question(models.Model):
     """Question model"""
@@ -14,11 +17,18 @@ class Question(models.Model):
     category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Choice(models.Model):
     """Choice model - Foreign Key to Question model"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'ID = {self.id}'
+
 
 # done
